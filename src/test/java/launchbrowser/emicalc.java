16d8f1ef817d66c89e3a3f89372ace53747d71ce
Deepak Hegadihal <vijay.diggavi@yahoo.com>
2017-06-03 15:53:07 +0530

    B2.0

--- src/test/java/launchbrowser/emicalc.java
+++ src/test/java/launchbrowser/emicalc.java
@@ -10,29 +10,34 @@ public class emicalc {
 	
 	@Test
 	public void bankapp() throws Exception{
-		 FirefoxDriver fd = new FirefoxDriver();
-		 
-		 Thread.sleep(10000);
-		  fd.get("https://www.allahabadbank.in/english/emi_calculator.aspx");
 		
-//		enter amount
-		  fd.findElement(By.id("one")).sendKeys("10000");
-		 //comment
-		
-//		Enter rate of intr=erest
-		  fd.findElement(By.id("two")).sendKeys("10");
-		
-//		enter tenure
-		  fd.findElement(By.id("three")).sendKeys("120");
-		
-//		Click on calcuate
-		  fd.findElement(By.name("B1")).click();  
-		  
-		
-//		extract EMI
-		  String output = fd.findElement(By.id("four")).getAttribute("value");
-		  
-		System.out.println(output);
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("VALUE OF C="+c);
+//		 FirefoxDriver fd = new FirefoxDriver();
+//		 
+//		 Thread.sleep(10000);
+//		  fd.get("https://www.allahabadbank.in/english/emi_calculator.aspx");
+//		
+////		enter amount
+//		  fd.findElement(By.id("one")).sendKeys("10000");
+//		 //comment
+//		
+////		Enter rate of intr=erest
+//		  fd.findElement(By.id("two")).sendKeys("10");
+//		
+////		enter tenure
+//		  fd.findElement(By.id("three")).sendKeys("120");
+//		
+////		Click on calcuate
+//		  fd.findElement(By.name("B1")).click();  
+//		  
+//		
+////		extract EMI
+//		  String output = fd.findElement(By.id("four")).getAttribute("value");
+//		  
+//		System.out.println(output);
 
 	}
 
